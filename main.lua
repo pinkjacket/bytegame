f3f2739254afc5e0bb2298e597564cb9881e20dd
James Donlan <jacketlizard@live.com>
2019-03-27 23:25:20 -0700

    draws the image in a random spot on every frame

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 function love.load()
-  
+  image = love.graphics.newImage("img/seal.jpg")
 end
 
 function love.update(dt)
@@ -7,5 +7,5 @@ function love.update(dt)
 end
 
 function love.draw()
-  
+  love.graphics.draw(image, love.math.random(0, 800), love.math.random(0, 600))
 end

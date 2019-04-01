36367b2b88953c2cd721b5130668b6aa8d1c01e0
James Donlan <jacketlizard@live.com>
2019-03-31 20:37:51 -0700

    input library installed, clicking the mouse and/or holding spacebar now print messages to console

--- main.lua
+++ main.lua
@@ -1,15 +1,21 @@
 Object = require "assets/lib/classic/classic"
+Input = require "assets/lib/input/Input"
 require "assets/obj/circle"
 require "assets/obj/hypercircle"
 assets = require("assets/lib/cargo/cargo").init("assets")
 
 function love.load()
   circletest = HyperCircle(400, 300, 50, 10, 120)
+  input = Input()
+  input:bind("mouse1", "test")
+  input:bind("space", "test2")
 end
 
 
 function love.update(dt)
-  
+  if input:pressed("test") then print("pressed") end
+  if input:released("test") then print("released") end
+  if input:down("test2", 0.5) then print("test event") end
 end
 
 function love.draw()
